d31ef1b6224d74763c736e8244ac464eed11364e
jupegarnica <jupegarnica@gmail.com>
2014-08-13 12:42:00 +0200

    working with a background daemon and logging

--- bin/daemon.sh
+++ bin/daemon.sh
@@ -5,18 +5,18 @@ HUBOT_ROOT="$USER_HOME/sherpabot"
 HUBOT_HOME="$HUBOT_ROOT"
 DAEMON="$HUBOT_HOME/bin/hubot"
 PIDFILE=$HUBOT_ROOT/hubot.pid
-
+touch $HUBOT_ROOT/hubot.log
 case "$1" in
 start)
         echo "Starting"
         . $HUBOT_ROOT/bin/.hubotrc
-        /sbin/start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile -d $HUBOT_ROOT --exec $DAEMON
+        /sbin/start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile -d $HUBOT_ROOT --startas /bin/bash -- -c "exec $DAEMON >> $HUBOT_ROOT/hubot.log 2>&1"
         echo "."
         ;;
 debug)
         echo "Debug mode: no backgrounding"
         . $HUBOT_ROOT/bin/.hubotrc
-        /sbin/start-stop-daemon --start --pidfile $PIDFILE --make-pidfile -d $HUBOT_ROOT --exec $DAEMON
+        /sbin/start-stop-daemon --start --pidfile $PIDFILE --make-pidfile -d $HUBOT_ROOT --startas /bin/bash -- -c "exec $DAEMON  >> $HUBOT_ROOT/hubot.log 2>&1"
         echo "."
         ;;        
 stop)
@@ -28,7 +28,7 @@ restart)
         echo "Restarting"
         /sbin/start-stop-daemon --stop --pidfile $PIDFILE
         . $HUBOT_ROOT/bin/.hubotrc
-        /sbin/start-stop-daemon --start --pidfile $PIDFILE --make-pidfile --background -d $HUBOT_ROOT --exec $DAEMON
+        /sbin/start-stop-daemon --start --pidfile $PIDFILE --make-pidfile --background -d $HUBOT_ROOT --startas /bin/bash -- -c "exec $DAEMON >> $HUBOT_ROOT/hubot.log 2>&1"
         echo "."
         ;;
 
